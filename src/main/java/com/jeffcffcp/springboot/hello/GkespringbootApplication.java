e77659029583adefb183bdd6caed9d870c514a1d
jeffcf@gmail.com <jeffcf@gmail.com>
2021-08-14 20:51:21 +0800

    add log

--- src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
+++ src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
@@ -1,20 +1,17 @@
 package com.jeffcffcp.springboot.hello;
 
-import java.util.stream.Collectors;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class GkespringbootApplication {
 
 	public static void main(String[] args) {
+		System.out.print("--------------------------------GkespringbootApplication start");
+		
 		SpringApplication.run(GkespringbootApplication.class, args);
 		
-		System.out.print("--------------------------------start end");
+		System.out.print("--------------------------------GkespringbootApplication end");
 		
 	}
 
