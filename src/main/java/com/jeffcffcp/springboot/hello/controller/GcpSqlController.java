8a7f26b1941b2aa4dc450330e6cadb23a1615aa6
jeffcf@gmail.com <jeffcf@gmail.com>
2021-08-15 05:57:12 +0800

    remove

--- src/main/java/com/jeffcffcp/springboot/hello/controller/GcpSqlController.java
+++ src/main/java/com/jeffcffcp/springboot/hello/controller/GcpSqlController.java
@@ -23,7 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
