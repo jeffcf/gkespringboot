d339087af1ccbb736c926391624685b5a0bf5891
jeffcf@gmail.com <jeffcf@gmail.com>
2021-08-15 06:15:01 +0800

    remove log

--- src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
+++ src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
@@ -1,22 +1,18 @@
 package com.jeffcffcp.springboot.hello;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GkespringbootApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(GkespringbootApplication.class);
+
 	
 	public static void main(String[] args) {
-		logger.info("--------------------------------GkespringbootApplication start");
-		
+
 		SpringApplication.run(GkespringbootApplication.class, args);
 		
-		logger.info("--------------------------------GkespringbootApplication end");
-		
+
 	}
 
 
