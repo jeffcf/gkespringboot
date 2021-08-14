7b9eab633509a7e78ec2a256fc17852c7521b43a
jeffcf@gmail.com <jeffcf@gmail.com>
2021-08-14 14:51:26 +0800

    fix

--- src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
+++ src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
@@ -3,6 +3,11 @@ package com.jeffcffcp.springboot.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author jeff
+ *
+ */
 @SpringBootApplication
 public class GkespringbootApplication {
 
