196a8b2537b789b1d15567d4e2c110b05cb79eec
jeffcf@gmail.com <jeffcf@gmail.com>
2021-08-15 10:36:02 +0800

    add log

--- src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
+++ src/main/java/com/jeffcffcp/springboot/hello/GkespringbootApplication.java
@@ -1,19 +1,21 @@
 package com.jeffcffcp.springboot.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GkespringbootApplication {
 
-
+	private static final Logger logger = LoggerFactory.getLogger(GkespringbootApplication.class);
 	
 	public static void main(String[] args) {
-		System.out.println("--------------------------------GkespringbootApplication start");
+		logger.info("--------------------------------GkespringbootApplication start");
 		
 		SpringApplication.run(GkespringbootApplication.class, args);
 		
-		System.out.println("--------------------------------GkespringbootApplication end");
+		logger.info("--------------------------------GkespringbootApplication end");
 		
 	}
 
